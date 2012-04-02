bb477e1d03aa404308e2f98ff860cb1f2a2f5d0e
HiNa <hina@bouhime.com>
2012-04-03 00:22:34 +0900

    fix typo

--- source/Tor/Jubeat/Copious/Playdata.php
+++ source/Tor/Jubeat/Copious/Playdata.php
@@ -4,7 +4,7 @@
  *
  * LICENSE
  *
- * This source file is subject to the 2-cause BSD License(Simplified
+ * This source file is subject to the 2-clause BSD License(Simplified
  * BSD License) that is bundled with this package in the file LICENSE.
  * The license is also available at this URL:
  * https://github.com/fetus-hina/Tor/blob/master/LICENSE
